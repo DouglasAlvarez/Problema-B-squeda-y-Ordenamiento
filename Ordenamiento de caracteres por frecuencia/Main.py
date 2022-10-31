9766840ca56659bbd30d985f5882abef248b05de
DouglasAlvarez <62295706+DouglasAlvarez@users.noreply.github.com>
2022-10-31 16:57:38 -0500

    Algoritmo de Ordenamiento por Frecuencia

---
+++ Ordenamiento de caracteres por frecuencia/Main.py
@@ -0,0 +1,22 @@
+from gene import Generador_texto
+
+class Solution:
+
+    def frequencySort(self, s: str) -> str:
+        output = []
+        for letter in set(s):
+            count = s.count(letter)  
+            output.append(letter*count) 
+        output = sorted(output, key=len, reverse = True) 
+        return "".join(output)
+    
+for i in range (10):  
+      
+    mi_texto = Generador_texto()
+    texto_aorden = mi_texto.generador_texto_aleatorio() 
+    print("caso "+ str(i)+": " + str(texto_aorden) ) 
+           
+    solucion = Solution()
+    palabra_orden = solucion.frequencySort(texto_aorden)
+    print("resultado caso "+ str(i)+": " + str(palabra_orden))
+    print("\n") 
